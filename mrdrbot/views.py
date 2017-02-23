75027dd88d17237d93f68a0e24b13b257b6347d3
Tobias Murphy <murphy.tobias@gmail.com>
2017-02-22 16:35:57 -0800

    deploying heroku

--- mrdrbot/views.py
+++ mrdrbot/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render, redirect
+import subprocess
+import sys
+import mrdrbot.singleton
+
 
 # Create your views here.
 def index(request):
     return render(request, 'mrdrbot/index.html')
+
+
+def startbot(reqest):
+    mrdrbot.singleton.proc = subprocess.Popen([sys.executable, '/Users/tobiasmurphy/code/mrdrbotsite/mrdrbot.py'])
+    # print(djangotwitter.singleton.proc)
+    return redirect('index')
+
+def stopbot(request):
+    mrdrbot.singleton.proc.kill()
+    return redirect('index')
