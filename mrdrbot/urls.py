1ac40bb50fc3124fa7ec865355fbf7d063dd6c2d
Tobias Murphy <murphy.tobias@gmail.com>
2017-02-23 11:00:14 -0800

    fixing typos

--- mrdrbot/urls.py
+++ mrdrbot/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
   url(r'^$', views.index, name="index"),
   url(r'^startbot$', views.startbot, name="startbot"),
   url(r'^stopbot$', views.stopbot, name="stopbot"),
-  url(r'^aobut$', views.about, name="about")
+  url(r'^about$', views.about, name="about")
 ]
