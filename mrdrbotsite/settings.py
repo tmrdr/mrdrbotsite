767327c82b346566e6bfaa2bc66dd0bee894cdd8
Tobias Murphy <murphy.tobias@gmail.com>
2017-02-23 17:00:56 -0800

    trying to get bot back under control

--- mrdrbotsite/settings.py
+++ mrdrbotsite/settings.py
@@ -22,7 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("djangokey")
+SECRET_KEY = "aaaaardfghj"
+
+# os.environ.get("djangokey")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
