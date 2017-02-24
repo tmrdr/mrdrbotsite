c673dfd93b6cdc8bb813ae60f16bb39b8829d0de
Tobias Murphy <murphy.tobias@gmail.com>
2017-02-24 10:09:12 -0800

    toggled added

--- mrdrbot/admin.py
+++ mrdrbot/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import TextFood
+from .models import TextFood, Toggled
 
 
 # Register your models here.
 admin.site.register(TextFood)
+admin.site.register(Toggeled)
